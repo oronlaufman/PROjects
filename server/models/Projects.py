d6740e7f124a8af5d429c586d67c674f94859702
iamoronlaufman <oronln1@gmail.com>
2020-12-03 17:17:54 +0200

    Project models

---
+++ server/models/Projects.py
@@ -0,0 +1,30 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from server.models import db
+
+
+class Company(db.Model):
+    __tablename__ = 'Companies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(120), nullable=False)
+    junior_name = db.Column(db.Array, nullable=False)
+    description = db.Column(db.Text)
+    field = db.Column(db.Array) 
+    status = db.Column(db.String(120))
+    password_hash = db.Column(db.String(120), nullable=False)
+
+    def __init__(self,company_name ,junior_name , description, field, status):
+        self.company_name = company_name
+        self.junior_name = junior_name
+        self.field = field
+        self.status = status
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
