d00ba54c1bc69cc0ccf7c09f6452d2eefa4e60a9
lilahz <lilahz05@gmail.com>
2020-12-18 11:21:58 +0200

    added new client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React from "react";
-import { render } from "react-dom";
-
-//function App() {
-//  return (
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//      </header>
-//    </div>
-//  );
-//}
-
-//export default App;
-
-const App = () => {
-    return (
-        <div>
-        <h1>Hello world</h1>
-        </div>
-    )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-render(<App />, document.getElementById("root"));
+export default App;
