7d5baac3555a5bd4899e106da411b8e48fa967d2
ayeletbiran <ayeletbiran3@gmail.com>
2020-12-21 10:19:03 +0200

    changes in querys for project

--- server/models/Company.py
+++ server/models/Company.py
@@ -1,5 +1,7 @@
+from flask.globals import session
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.orm import load_only
 
 from server.models import db
 
@@ -31,6 +33,10 @@ class Company(UserMixin, db.Model):
     def add_new_company(new_company):
         db.session.add(new_company)
         db.session.commit()
+
+    def get_company_name_by_id(id):
+        result = db.session.query(Company.company_name).filter(Company.id==id).first()
+        return result[0]
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
