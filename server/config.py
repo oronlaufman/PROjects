399748f44036674051a3519063900cedbb6d36c2
oronlaufman <57946539+oronlaufman@users.noreply.github.com>
2021-02-15 19:09:02 +0200

    Update config.py

--- server/config.py
+++ server/config.py
@@ -26,7 +26,7 @@ class ProductionConfig(Config):
     Configurations for the production environment - external environment
     """
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', HEROKU_POSTGRES)
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://tjixmnlsigtcwm:ccbaf7688761f2193b6c9f46f14d0ac92fa9a439edd32925e21eb36c379e12fa@ec2-34-203-155-237.compute-1.amazonaws.com:5432/df415d7mbtsmil')
 
 
 config_by_mode = dict(dev=DevelopmentConfig, prod=ProductionConfig)
